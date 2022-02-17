68884bd6fb515b5f021ba001baeb8b0efef33ebd
Ceyda <ceydaonemli2@gmail.com>
2022-02-17 21:37:12 +0300

    Book.css and BookList.css created

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import './Book.css';
 class Book extends React.Component {
     render() {
-        console.log(this.props.book)
-        return <div>
-            <h2> {this.props.book.title}</h2>
-            <h4> {this.props.book.author}</h4>
+        //console.log(this.props.book)
+        return (
+            <div className="col-lg-4 col-sm-6">
 
-
-        </div>
+                <div className="portfolio-item">
+                    <a className="portfolio-link" data-bs-toggle="modal" href="#portfolioModal1">
+                        <div className="portfolio-hover">
+                            <div className="portfolio-hover-content"> <i class="fas da-plus fa-3x"></i> </div>
+                        </div>
+                        <img className="img-fluid" src={this.props.book.imageURL} alt="" />
+                    </a>
+                    <div className="portfolio-caption">
+                        <div className="portfolio-caption-heading">{this.props.book.title}</div>
+                        <div className="portfolio-caption-subheading text-muted">{this.props.book.author}</div>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 export default Book;
+
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import Book from './Book';
+import './BookList.css'
 class BookList extends React.Component {
     render() {
         //console.log(this.props.books);
 
-        const booklist = this.props.books.map((book, i ) => {
-            return <Book book={book}  
+        const booklist = this.props.books.map((book, i) => {
+            return <Book book={book}
                 key={i}
             />
         })
-        return <div>{booklist}</div>
+        return (
+            <section className="page-section bg-light" id="portfolio">
+                <div className="container">
+                    <div className="text-center">
+                        <h2 className="section-heading text-uppercase">BookFolio</h2>
+                        <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    </div>
+                    <div className="row">
+                        {booklist}
+                    </div>
+                </div>
+            </section>
+        )
     }
 }
 export default BookList;
+
+
